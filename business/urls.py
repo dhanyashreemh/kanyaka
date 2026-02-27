e41ab27a3e101f22d827fcab410b1ce6a4be82a4
dhanyashreemh <dhanyashree@raakacloud.com>
2026-02-27 11:17:50 +0530

    .

--- business/urls.py
+++ business/urls.py
@@ -1,7 +1,10 @@
+# urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("dashboard/", views.dashboard, name="dashboard"),
-    path("update-rate/", views.update_rate, name="update_rate"),
+ 
+    path("staff/dashboard/", views.dashboard, name="dashboard"),
+    path("staff/update-rate/", views.update_rate, name="update_rate"),
+    path("api/gold-rate/", views.gold_rate_api, name="gold_rate_api"),
 ]
