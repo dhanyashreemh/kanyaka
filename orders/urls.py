c918c131b5620eb94200b2721f086dcd89ac197d
dhanyashreemh <dhanyashree@raakacloud.com>
2026-03-01 17:10:13 +0530

    .

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import shopify_webhook
 
 urlpatterns = [
-    path("shopify/", shopify_webhook, name="shopify_webhook"),
+    path("webhook/order/", shopify_webhook, name="shopify_webhook"),
 ]
