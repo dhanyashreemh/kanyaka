6665bb514da87fd07373486e89c5a0c5edd64b58
dhanyashreemh <dhanyashree@raakacloud.com>
2026-03-09 13:58:47 +0530

    .

--- products/models.py
+++ products/models.py
@@ -4,7 +4,18 @@ class Product(models.Model):
     shopify_product_id = models.CharField(max_length=255, unique=True)
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    raw_data = models.JSONField()
+    collection = models.CharField(max_length=100, blank=True, null=True)
+    compare_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True, null=True)
+    jewelry_type = models.CharField(max_length=100, blank=True, null=True)
+    metal_type = models.CharField(max_length=100, blank=True, null=True)
+    occasion = models.CharField(max_length=100, blank=True, null=True)
+    purity = models.CharField(max_length=20, blank=True, null=True)
+    quantity = models.IntegerField(default=0)
+    sku = models.CharField(max_length=100, blank=True, null=True)
+    stone_type = models.CharField(max_length=100, blank=True, null=True)
+    weight = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
         return self.title
